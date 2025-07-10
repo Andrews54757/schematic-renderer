c91369ca55237f7e3bdcb0386b8901cfd067d3c6
Andrew S <andrews54757@gmail.com>
2025-07-09 22:14:46 -0400

    Undo

--- src/managers/SchematicObject.ts
+++ src/managers/SchematicObject.ts
@@ -115,22 +115,21 @@ export class SchematicObject extends EventEmitter {
 		);
 
 		const schemBounds = this.getSchematicBounds();
-		const dimensions = this.getDimensions();
 
 		if (properties?.meshBoundingBox) {
 			this.meshBoundingBox = properties.meshBoundingBox;
 		} else {
 			this.meshBoundingBox = [
-				[0,0,0],
-				[dimensions[0], dimensions[1], dimensions[2]]
+				[schemBounds[0][0], schemBounds[0][1], schemBounds[0][2]],
+				[schemBounds[1][0], schemBounds[1][1], schemBounds[1][2]],
 			];
 		}
 
 		// Initialize rendering bounds to the full schematic dimensions
 		// But they are disabled by default (they won't be used for culling unless explicitly enabled)
 		this.renderingBounds = {
-			min: new THREE.Vector3(0,0,0),
-			max: new THREE.Vector3(dimensions[0], dimensions[1], dimensions[2]),
+			min: new THREE.Vector3(schemBounds[0][0], schemBounds[0][1], schemBounds[0][2]),
+			max: new THREE.Vector3(schemBounds[1][0], schemBounds[1][1], schemBounds[1][2]),
 			enabled: false, // Disabled by default
 		};
 
